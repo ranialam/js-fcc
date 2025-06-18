35d19d14d6adb1c02338f5bb10a400ced8aec1c3
ranialam <lamine-rania@outlook.com>
2025-06-18 23:01:41 +0100

    increment done, save B 1st try

---
+++ peoplecounter-js.js
@@ -0,0 +1,11 @@
+let number=0;
+    function incrementfunc(){
+      number=number+1;
+      alert("the number of people is :" +number);
+      document.getElementById("countdisplay").textContent=" People count " +number;
+
+    }
+
+    function savingfunc(){
+      alert("you've saved the number of people");
+    }
